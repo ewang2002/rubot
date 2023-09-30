625e2bedfe923d3358b74f6655509de338ab53a9
Mrat <78581772+mr4tt@users.noreply.github.com>
2023-09-29 19:04:03 -0700

    added bash script to run prod vs test

--- run.sh
+++ run.sh
@@ -1,1 +1,23 @@
-rm -r out && clear && npm run compile && clear && npm run start
+set -e
+
+if [[ "$1" == "test" ]]; then
+    version="config.test.json"
+    if [[ ! -f "$version" ]]; then
+        echo "ERROR: TEST FILE NOT FOUND"
+        exit 1
+    fi 
+else
+    version="config.production.json"
+    if [[ ! -f "$version" ]]; then
+        echo "ERROR: PROD FILE NOT FOUND"
+        exit 1
+    fi 
+fi
+
+rm -rf out # removes old commands, puts in updated ones 
+clear
+
+npm run compile
+clear
+
+npm run start -- $version
