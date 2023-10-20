e8d8396887f46d4c1d4294154577a7f7a28b9e77
Mrat <78581772+mr4tt@users.noreply.github.com>
2023-10-19 21:25:26 -0700

    adding fields for postgres on config type

--- src/definitions/ConfigTypes.ts
+++ src/definitions/ConfigTypes.ts
@@ -48,6 +48,9 @@ export interface IConfiguration {
         user: string;
         password: string;
         host: string;
+        port: number;
+        ssl: boolean,
+        database: string;
     };
 }
 
