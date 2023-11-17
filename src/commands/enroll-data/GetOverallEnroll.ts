7f82602bf62f3153470b31bfb4db7e9c874b9b24
Mrat <78581772+mr4tt@users.noreply.github.com>
2023-11-16 20:55:50 -0800

    changing overallplot cmd to loop thru quarters and show last n graphs

--- src/commands/enroll-data/GetOverallEnroll.ts
+++ src/commands/enroll-data/GetOverallEnroll.ts
@@ -24,12 +24,14 @@ export default class GetOverallEnroll extends BaseCommand {
      * @inheritDoc
      */
     public async run(ctx: ICommandContext): Promise<number> {
-        const term = ctx.interaction.options.getString("term", true);
         const code = ctx.interaction.options.getString("course_subj_num", true);
+        const term = ctx.interaction.options.getString("term", false) ?? "all";
         const searchType = ctx.interaction.options.getString("search_type", false) ?? "norm";
 
         let coll: Readonly<Collection<string, IPlotInfo[]>>;
         let display: string;
+        const parsedCode = parseCourseSubjCode(code);
+
         switch (searchType) {
             case "wide":
                 coll = DataRegistry.OVERALL_ENROLL_WIDE;
@@ -42,33 +44,58 @@ export default class GetOverallEnroll extends BaseCommand {
                 break;
         }
 
-        const arr = coll.get(term);
-        if (!arr) {
-            await ctx.interaction.reply({
-                content: `The term, **\`${term}\`** (Display \`${display}\`), could not be found. Try again.`,
-                ephemeral: true,
-            });
+        let arr;
+        let res;
+        const filesList = [];
+        const termsList = [];
+        if (term !== "all") {
+            arr = coll.get(term);
+            if (!arr) {
+                await ctx.interaction.reply({
+                    content: `The term, **\`${term}\`** (Display \`${display}\`), could not be found. Try again.`,
+                    ephemeral: true,
+                });
+    
+                return -1;
+            }
 
-            return -1;
+            res = arr.find((x) => x.fileName === parsedCode);
+            if (!res) {
+                await ctx.interaction.reply({
+                    content:
+                        `The course, **\`${parsedCode}\`**, (term **\`${term}\`** & display \`${display}\`) could not` +
+                        " be found. Try again.",
+                    ephemeral: true,
+                });
+    
+                return -1;
+            }
+            filesList.push(res);
+            termsList.push(term);
         }
-
-        const parsedCode = parseCourseSubjCode(code);
-        const res = arr.find((x) => x.fileName === parsedCode);
-        if (!res) {
-            await ctx.interaction.reply({
-                content:
-                    `The course, **\`${parsedCode}\`**, (term **\`${term}\`** & display \`${display}\`) could not` +
-                    " be found. Try again.",
-                ephemeral: true,
+        else {
+            const terms = DataRegistry.CONFIG.ucsdInfo.githubTerms.map((x) => {
+                return { name: x.termName, value: x.term };
             });
 
-            return -1;
+            for (const testTerm of terms.slice(0, 6)) {
+                arr = coll.get(testTerm.value);
+                if (arr) {
+                    const info = arr.find((x) => x.fileName === parsedCode);
+                    if (info) {
+                        filesList.push(info);
+                        termsList.push("- " + testTerm.name + "\n");
+                    }
+                }
+            }
         }
 
         await ctx.interaction.deferReply();
         await ctx.interaction.editReply({
-            files: [res.fileUrl],
-            content: `Course **\`${parsedCode}\`** (Term **\`${term}\`**, Display \`${display}\`)`,
+            files: filesList.map((x) => {
+                return x.fileUrl;
+            }),
+            content: `**\`${parsedCode}\`**\n -----------------\nTerms: \n**\`${termsList.join("")}\`**`,
         });
 
         return 0;
