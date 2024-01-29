50e045d2d3fca74ba90c9c0d12bea56d3b64bc3e
mrat <78581772+mr4tt@users.noreply.github.com>
2024-01-29 13:15:32 +0800

    fixing database / schema names

--- createTables.sql
+++ createTables.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE rubot;
 
-CREATE SCHEMA IF NOT EXISTS "roobot";
+CREATE SCHEMA IF NOT EXISTS "rubot";
 
-CREATE TABLE IF NOT EXISTS "roobot"."watch" (
+CREATE TABLE IF NOT EXISTS "rubot"."watch" (
     "user_id" text NOT NULL,
     "course" text NOT NULL,
     "channel_id" text NOT NULL,
     "previously_open" boolean NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "roobot"."alert" (
+CREATE TABLE IF NOT EXISTS "rubot"."alert" (
     "id" SERIAL PRIMARY KEY,
     "user_id" text NOT NULL,
     "message" text NOT NULL,
